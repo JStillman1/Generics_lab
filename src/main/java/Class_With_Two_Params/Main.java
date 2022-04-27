6699255ae53f9cb2a93d8d7b4f32bed169c5dde9
James Stillman <jamesstillman99@gmail.com>
2022-04-27 14:47:11 +0100

    Pair methods tested in main method

---
+++ src/main/java/Class_With_Two_Params/Main.java
@@ -0,0 +1,16 @@
+package Class_With_Two_Params;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pair<String, Integer> pair1 = new Pair<>("", 10);
+        Pair<Integer, String> pair2 = new Pair<>(20, "");
+        Pair<String, String> pair3 = new Pair<>("", "");
+
+        System.out.println(pair1.getV());
+        System.out.println(pair2.getK());
+        pair3.setK("Test");
+        System.out.println(pair3.getK());
+
+    }
+}
