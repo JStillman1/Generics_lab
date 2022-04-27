743dc6ac04c63e789e8f941bac86880659345e1b
James Stillman <jamesstillman99@gmail.com>
2022-04-27 14:44:42 +0100

    Pair methods implemented

--- src/main/java/Class_With_Two_Params/Pair.java
+++ src/main/java/Class_With_Two_Params/Pair.java
@@ -2,7 +2,31 @@ package Class_With_Two_Params;
 
 public class Pair <K, V>{
 
-    
+    private K k;
+    private V v;
+
+    public Pair(K k, V v){
+        this.k = k;
+        this.v = v;
+    }
 
 
+
+
+    //Getters and Setters
+    public K getK() {
+        return k;
+    }
+
+    public void setK(K k) {
+        this.k = k;
+    }
+
+    public V getV() {
+        return v;
+    }
+
+    public void setV(V v) {
+        this.v = v;
+    }
 }
