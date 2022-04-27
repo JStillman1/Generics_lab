23eb1026401b3661f7907618634fbe274871355a
James Stillman <jamesstillman99@gmail.com>
2022-04-27 14:36:41 +0100

    Box class created

---
+++ src/main/java/Create_Simple_Class/Box.java
@@ -0,0 +1,13 @@
+package Create_Simple_Class;
+
+public class Box <T> {
+    private T t;
+    
+    public T get(){
+        return this.t;
+    }
+    
+    public void set(T t){
+        
+    }
+}
