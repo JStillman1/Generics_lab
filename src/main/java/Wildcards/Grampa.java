d4ccf550f113f5119bcbd5c80b3f147401c01ae5
James Stillman <jamesstillman99@gmail.com>
2022-04-27 15:03:07 +0100

    Grampa class implemented

--- src/main/java/Wildcards/Grampa.java
+++ src/main/java/Wildcards/Grampa.java
@@ -1,5 +1,18 @@
 package Wildcards;
 
 public class Grampa {
-    
+
+    private String name;
+
+    public Grampa(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Grampa{" +
+                "name='" + name + '\'' +
+                "class='" + this.getClass() + "\'" +
+                '}';
+    }
 }
